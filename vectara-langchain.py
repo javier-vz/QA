6d02fc6358b3a2da721bb397910997a19c9785d9
Javier Vera Zúñiga <35106049+javiervz@users.noreply.github.com>
2024-08-07 19:33:07 -0500

    Create vectara-langchain.py

---
+++ vectara-langchain.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug  7 19:04:03 2024
+
+@author: u_humanidades
+"""
+
+from langchain_community.vectorstores import Vectara
+from langchain_community.vectorstores.vectara import (
+    RerankConfig,
+    SummaryConfig,
+    VectaraQueryConfig,
+)
+
+vectara = Vectara(
+                vectara_customer_id="2620549959",
+                vectara_corpus_id=2,
+                vectara_api_key="zwt_nDJrRzeXqkymaH-tptpheMyOCzm4oL_NKD9hOw"
+            )
+
+summary_config = SummaryConfig(is_enabled=True, max_results=7, response_lang="spa")
+rerank_config = RerankConfig(reranker="mmr", rerank_k=50, mmr_diversity_bias=0.2)
+config = VectaraQueryConfig(
+    k=10, lambda_val=0.005, rerank_config=rerank_config, summary_config=summary_config
+)
+
+query_str = "Por qué los danzantes usan máscaras?"
+
+rag = vectara.as_rag(config)
+print(rag.invoke(query_str)["answer"])
