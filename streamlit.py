93298048cda32d01d96d2a7a541fbfe9c2ad6688
Javier Vera Zúñiga <35106049+javiervz@users.noreply.github.com>
2024-08-07 20:12:30 -0500

    streamlit

--- streamlit.py
+++ streamlit.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Aug  7 19:04:03 2024
+Created on Wed Aug  7 19:59:09 2024
 
 @author: u_humanidades
 """
@@ -22,13 +22,18 @@ vectara = Vectara(
 summary_config = SummaryConfig(is_enabled=True, max_results=5, response_lang="spa")
 rerank_config = RerankConfig(reranker="mmr", rerank_k=50, mmr_diversity_bias=0.1)
 config = VectaraQueryConfig(
-    k=10, lambda_val=0.0075, rerank_config=rerank_config, summary_config=summary_config
+    k=10, lambda_val=0.005, rerank_config=rerank_config, summary_config=summary_config
 )
 
-query_str = "¿Por qué los danzantes usan máscaras?"
+import streamlit as st
+ 
+st.write("""
+# Prototipo de *buscador!*
+""")
+
+query_str = st.chat_input("Pregunta algo sobre las festividades de Paucartambo!")
 
 rag = vectara.as_rag(config)
 resp = rag.invoke(query_str)
-print(resp["answer"])
-print(f"Vectara FCS = {resp['fcs']}")
 
+st.write("La IA piensa que: " + resp["answer"])
